6970b973122bc0cc4df9782ae03aff1a95c1c2f2
Mirko Mariotti <mirko@mirkomariotti.it>
2024-07-24 11:58:31 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate tempdir;
+use clap::Parser;
+
+use std::fs::File;
+use std::io::{self, Write};
+use tempdir::TempDir;
+/// The arguments for the program 
+#[derive(Parser)]
+struct Args {
+    /// The regression command to run
+    command: String,
+    /// The example to run
+    example: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let test_regression = "zedboard_bellstate";    
+    if let Err(_) = execute_regression(test_regression) {
+        ::std::process::exit(1);
+    }
+}
+
+fn execute_regression( regression_name: &str) -> Result<(), io::Error> {
+    println!("Running regression: {}", regression_name);
+
+    let tmp_dir = TempDir::new("bmregression")?;
+    println!("Working directory: {}", tmp_dir.path().display());
+
+    let file_path = tmp_dir.path().join("my-temporary-note.txt");
+    let mut tmp_file = File::create(file_path)?;
+    writeln!(tmp_file, "Brian was here. Briefly.")?;
+    drop(tmp_file);
+    tmp_dir.close()?;
+    Ok(())
+}
